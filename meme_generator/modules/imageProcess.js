cf8f258d985917f0baf67c58fb1bc32055467f17
Rob Lum <lum.robert@gmail.com>
2015-05-22 17:51:18 -0400

    dev(meme generator): working on amazon elastic beanstalk; need to reorganize image folders and test from coffeescript req

---
+++ meme_generator/modules/imageProcess.js
@@ -0,0 +1,32 @@
+var gm              = require('gm');
+var imageMagick     = gm.subClass({ imageMagick: true });
+var appRoot         = require('app-root-path').path;
+var sendToS3        = require('./sendToS3');
+
+
+function filterRequest(req, res) {
+    req             = req.body;
+    var caption     = decodeURIComponent(req['caption']);
+    var image       = req['person'];
+    console.log(caption, image);
+    createImage(caption, image, res);
+}
+
+function createImage(caption, image, res) {
+    var originalImage   = '/images/' + image + '.jpg';
+
+    imageMagick(appRoot + originalImage)
+        .fill("white")
+        .stroke('black', [1])
+        .fontSize(35)
+        .drawText(0, 100, caption, 'South')
+        .stream(function(err, stdout, stderr) {
+            if (err) {
+                res.send('Unexpected error: Stream err');
+            } else if (stdout) {
+                sendToS3(err, stdout, stderr, originalImage, res);
+            }
+        });
+}
+
+module.exports = filterRequest;
---
+++ meme_generator/modules/sendToS3.js
@@ -0,0 +1,50 @@
+var AWS             = require('aws-sdk');
+var mime            = require('mime');
+var shortId         = require('shortid');
+var dotenv          = require('dotenv');
+
+dotenv.load();
+var S3_key          = process.env.AWS_SECRET_KEY;
+var S3_secret       = process.env.AWS_SECRET_KEY_ID;
+var S3_bucket       = process.env.S3_BUCKET_NAME;
+
+AWS.config.update({
+    accessKeyId: S3_key,
+    secretAccessKey: S3_secret
+});
+
+var s3 = new AWS.S3();
+
+
+function sendToS3(err, stdout, stderr, originalImage, client_response) {
+    var imageName       = shortId.generate();
+    var buff            = new Buffer('');
+
+    var BASE_URL        = 'https://s3.amazonaws.com/assets.offerpop.com/';
+    var linkPath        = 'roblum/meme_generator/' + imageName + '.jpg';
+
+    stdout.on('data', function(data) {
+        buff = Buffer.concat([buff, data]);
+    });
+
+    stdout.on('end', function(data) {
+        var data = {
+                Bucket      : S3_bucket,
+                Key         : linkPath,
+                Body        : buff,
+                ContentType : mime.lookup(originalImage)
+        };
+
+        s3.putObject(data, function(err, res) { // Folder to store linkable images
+            if (err) {
+                console.log('There was an error: ' + err);
+            } else {
+                client_response.send({
+                    link: BASE_URL + linkPath,
+                });
+            }
+        });
+    });
+}
+
+module.exports = sendToS3;
